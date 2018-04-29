ad0bc935513a76be66a9ad7e9b6bccd5f3c803a9
PlatyPrograms <eric.barclay.andrews@gmail.com>
2018-04-29 18:24:16 -0400

    removed tail

--- mpiDecompress.c
+++ mpiDecompress.c
@@ -8,19 +8,17 @@ void getMetaData(FILE* meta, FILE* data, unsigned char* mUsed,
                  unsigned char* dUsed, unsigned char* mCur,
                  unsigned char* dCur, unsigned char* runLen,
                  unsigned char* keyLen, uint64_t* numRuns,
-		 unsigned char* expProcs, unsigned char* isTail) {
+		 unsigned char* expProcs) {
   // gets data out of the beginning of the meta, data files on the bit
   // lengths of keys and the number of runs
   *mUsed = *dUsed = 0;
   *mCur = fgetc(meta);
   *dCur = fgetc(data);
   unsigned char expProcsSize = 8; // should be 1 bit, but time
-  unsigned char tailSize = 8; // should be 1 bit, but time
   unsigned char tagSize = 8; // should be 6 bits, but time
   unsigned char numRunSize = 48;
   
   *expProcs = get(meta, mUsed, mCur, expProcsSize);
-  *isTail = get(meta, mUsed, mCur, tailSize);
   *numRuns = get(meta, mUsed, mCur, numRunSize);
   *runLen = (char) get(meta, mUsed, mCur, tagSize);
   *keyLen = (char) get(data, dUsed, dCur, tagSize);
@@ -101,11 +99,10 @@ int main(int argc, char** argv) {
 
   // variables for tracking relevant features of files
   uint64_t numRuns;
-  unsigned char expProcs, mUsed, dUsed, mCur, dCur, runLen, keyLen,
-    isTail;
+  unsigned char expProcs, mUsed, dUsed, mCur, dCur, runLen, keyLen;
   
   getMetaData(meta, data, &mUsed, &dUsed, &mCur, &dCur, &runLen, &keyLen,
-              &numRuns, &expProcs, &isTail);
+              &numRuns, &expProcs);
   if (nProc < expProcs) {
     printf("ERROR: expected %i processes, got %i\n", expProcs, nProc);
     MPI_Abort(MPI_COMM_WORLD, MPI_ERR_RANK);
@@ -120,7 +117,7 @@ int main(int argc, char** argv) {
     }
   }
   // reset meta
-  fseek(meta, 9, SEEK_SET);
+  fseek(meta, 8, SEEK_SET);
   mUsed = 0;
   mCur = fgetc(meta);
   
